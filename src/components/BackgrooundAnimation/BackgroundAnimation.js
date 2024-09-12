4d60d9e986c341400423eaae140efa76731db7c7
andyh98 <hameea1@mcmaster.ca>
2024-09-11 23:32:33 -0400

    orange circle json lottie anim

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import Circle from "./circle.json";
 
 const BackgroundAnimation = () => (
   <div>
-  
+    <Lottie loop={false} animationData={Circle}></Lottie>
   </div>
 );
 
