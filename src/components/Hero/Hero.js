1afbfa1943068125753d01adf2d1e4e2ace40cce
Andy Hameed <36431131+andyh98@users.noreply.github.com>
2024-10-20 12:48:01 -0400

    Hero contact me to view CV

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
       A Software Engineer who is passionate about helping people live better lives through technology
       </SectionText>
       
-      <a href="mailto:andyhameed@outlook.com"><Button>Contact Me</Button></a>
+      <a href="https://drive.google.com/file/d/1WBQGVJFu3Q4aRzt7pIjoV_G_d-E5GSZV/view?usp=sharing"><Button>View CV</Button></a>
     </LeftSection>
   </Section>
 );
