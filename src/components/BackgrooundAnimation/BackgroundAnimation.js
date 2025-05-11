514e07e286a6a5adae204c6473eb92901b76efb2
Andy Hameed <36431131+andyh98@users.noreply.github.com>
2025-05-11 12:51:28 -0400

    back to previous setup

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -3,10 +3,9 @@ import Lottie from "lottie-react";
 import Circle from "./circle.json";
 
 const BackgroundAnimation = () => (
-  // <div>
-  //   <Lottie loop={false} animationData={Circle}></Lottie>
-  // </div>
-  <div style="width:300px;margin:0 auto;"><div style="text-align:center"><span style="font:9px/14px arial; color:#3d3d3d;">Powered By <a target="_blank" id="vt_link" rel="nofollow" href="http://www.varsitytutors.com/tutors/880278933/">Varsity Tutors</a></span></div><iframe id="vt_content_frame" allowtransparency="true" style="height:520px; width:300px" frameborder="0" src="//widgets.varsitytutors.com/tutors/880278933/badge?vt_theme=vt1"></iframe></div>
+  <div>
+    <Lottie loop={false} animationData={Circle}></Lottie>
+  </div>
 );
 
 export default BackgroundAnimation;
